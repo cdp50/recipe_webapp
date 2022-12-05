fe6bf16a1f7bb1e9fca876f063daf8d9c229f718
cdp50 <par17069@byui.edu>
2022-12-04 17:36:54 -0700

    done

---
+++ README.md
@@ -0,0 +1,28 @@
+# Overview
+
+Since I'm studying software engineering I got curious about how a Web App framework actually works so I decided to build my first Web App using Django. I wanted to know the basics on how to develop a Web Application and how to make it communicate with an external API, to organize and publish data on it. 
+
+in order to test this Web App I have to open the terminal making sure that I am at the "proj" folder using the command `ls` I can see the list of files at the address that I'm in and using the command `cd {folder_name}` I can enter to the "proj folder" then type `python manage.py runserver` and the Web App is ready I just have to cmd click on the link and add the path to the url. 
+
+[Web App Demo Video](https://youtu.be/ZZsY6cIg0vM)
+
+# Web Pages
+
+This is a Web App that connects to an API called Spoonacular. I used it to build a website that search for a random recipe every time you click on Random Search this website is under the url random_search/. I also build another page that you can find under the url search/. This one retrieves one recipe that contains all the ingredients that you listed, so lets say that you want to cook something with these specific ingredients, you can just list them in this website click on search and you will get a recipe that includes all these ingredients. 
+
+# Development Environment
+
+The main tools to create this Web App are Django as the framework, Python for the programming language and Spoonacular for the API that I used to retrieve the information to display. It was satisfying to learn this on python because I only knew how to deal with API's using JavaScript. 
+
+# Useful Websites
+
+* [Django project](https://www.djangoproject.com/start/) - Official Django tutorial
+* [Django documentation](https://docs.djangoproject.com/en/4.1/) - Official Django documentation
+* [Youtube](https://www.youtube.com/watch?v=PtQiiknWUcI&t=1963s) - Django python 7 hour course
+
+# Future Work
+
+* I will make it prettier. I would like to add much more css on it. 
+* I will add it more features. I want it to be more precise on the conditions that you can add to the search. so it can search for a recipe that only contains the ingredients list. or return a recipe with a specific amount of intake calories. 
+* I will add more pages as I get to add it more features. 
+* I wil learn how to manage API's in C#. 
--- proj/recipes/views.py
+++ proj/recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import requests
+
 # Create your views here.
 # Home of my Functions. 
 # admin.site.register("name of the model")
@@ -8,17 +9,16 @@ import requests
 def get_recipe_id (request):
     if request.method == "POST":
         g_key = "7c8a7ecdbb68451f85463ff992469769"
-        byui_key = "5a5313fdf6874562957e3fc267ae46cf"
     # how do I get the input from the user? using the request.POST will give me a dictionary with the form
         ingredients = request.POST["ingredients"].replace(" ","")
-        url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients="+ingredients+"&number=1&apiKey="+byui_key
+        url = "https://api.spoonacular.com/recipes/findByIngredients?ingredients="+ingredients+"&number=1&apiKey="+g_key
     # is this how I should fetch data from the API? looks like it
         server_data_id = requests.get(url).json()
         print(server_data_id)
         print(url)
         id = server_data_id[0]["id"]
         # get_recipe_img(id)
-        img_url = "https://api.spoonacular.com/recipes/"+str(id)+"/card?apiKey="+byui_key
+        img_url = "https://api.spoonacular.com/recipes/"+str(id)+"/card?apiKey="+g_key
         server_data_img = requests.get(img_url).json()
         recipe_img = {"recipe_img" : server_data_img["url"]}
         print(server_data_img)
@@ -28,13 +28,11 @@ def get_recipe_id (request):
 def get_random_recipe_id(request):
     if request.method == "POST":
         g_key = "7c8a7ecdbb68451f85463ff992469769"
-        byui_key = "5a5313fdf6874562957e3fc267ae46cf"
-        url = "https://api.spoonacular.com/recipes/random?number=1&apiKey="+byui_key
+        url = "https://api.spoonacular.com/recipes/random?number=1&apiKey="+g_key
         server_data_id = requests.get(url).json()
- 
         print(server_data_id["recipes"][0]["id"])
         id = server_data_id["recipes"][0]["id"] #this is giving the id fine
-        img_url = "https://api.spoonacular.com/recipes/"+str(id)+"/card?apiKey="+byui_key
+        img_url = "https://api.spoonacular.com/recipes/"+str(id)+"/card?apiKey="+g_key
         server_data_img = requests.get(img_url).json()
         print("server_data")
         print("server_data")
@@ -50,17 +48,6 @@ def get_random_recipe_id(request):
     # How do I show to the user this results?
 
 
-
-
-
-    # def get_recipe_img(id):
-#     g_key = "7c8a7ecdbb68451f85463ff992469769"
-#     byui_key = "5a5313fdf6874562957e3fc267ae46cf"
-#     img_url = "https://api.spoonacular.com/recipes/"+id+"/card?apiKey="+g_key
-#     server_data = requests.get(img_url).json()
-#     recipe_img = server_data.url
-    # how do I show the image to the user?
-    # and how can I make it smaller?
 
 
 
